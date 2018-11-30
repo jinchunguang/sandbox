babc04e9db684f6a79656baabc68bd53af41ba94
jinchunguang <jinchunguang@jiayunhui.com>
2018-11-30 20:00:40 +0800

    update file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\System\Jsonify;
+use Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +15,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        if (config('app.log_level') == 'debug') {
+
+            // sql检测
+            \DB::listen(
+                function ($db) {
+                    foreach ($db->bindings as $i => $binding) {
+                        if ($binding instanceof \DateTime) {
+                            $db->bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+                        } else {
+                            if (is_string($binding)) {
+                                $db->bindings[$i] = "'$binding'";
+                            }
+                        }
+                    }
+                    $query = str_replace(array('%', '?'), array('%%', '%s'), $db->sql);
+                    $query = vsprintf($query, $db->bindings);
+                    $args = implode(' | ', $db->bindings);
+                    Log::debug(" # sql:{$query} # time:{$db->time} # binds:{$args}");
+                }
+            );
+        }
     }
 
     /**
@@ -25,7 +47,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        $this->app->bind('Jsonify', function($app) {
+        $this->app->bind('Jsonify', function ($app) {
             return new Jsonify($app);
         });
     }
